e07badb177cb53fe264c8c15889cd6a3ae783117
spttmssla55 <spttmssla555@naver.com>
2025-05-12 14:25:04 +0900

    로그아웃 수정

---
+++ frontend/src/MyPage/Accommodation.js
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from "react";
+import { UserContext } from "../Session/UserContext";
+import EditPop from "./PopUp/EditPop";
+
+
+const Accommodation = () => {
+
+
+    return (
+        <div>
+            <h1>숙박 확인</h1>
+
+
+
+        </div>
+    );
+};
+
+export default Accommodation;
