48d78875e1012aaad41224d9938368a77123c209
OOOJJJHHH <torgeleli2@gmail.com>
2025-07-19 14:06:37 +0900

    250719 예약 기능 (1406 오준희)

--- OneProject/src/main/java/com/example/oneproject/controller/CityController.java
+++ OneProject/src/main/java/com/example/oneproject/controller/CityController.java
@@ -196,6 +196,8 @@ public class CityController {
     // 예약 ===========================================================================
     @PostMapping("/reservation")
     public ResponseEntity<?> createReservation(@RequestBody ReservationRequestDTO dto) {
+        System.out.println("받은 예약 요청: " + dto);
+
         Reservation saved = reservationService.createReservation(dto);
         Map<String, Object> response = new HashMap<>();
         response.put("success", true);
