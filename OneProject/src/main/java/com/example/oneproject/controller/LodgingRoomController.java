9024138bfb8bc0f01343b083b084435dcb99c737
OOOJJJHHH <torgeleli2@gmail.com>
2025-09-26 22:36:52 +0900

    250926 메인페이지 최저가 숙소 (2236 오준희)

--- OneProject/src/main/java/com/example/oneproject/controller/LodgingRoomController.java
+++ OneProject/src/main/java/com/example/oneproject/controller/LodgingRoomController.java
@@ -7,20 +7,21 @@ import com.example.oneproject.Service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/rooms")
 public class LodgingRoomController {
 
     @Autowired
     private RoomService roomService;
 
-    // 별점 순으로 출력(메인 페이지에서 활용)
+    // 가장 저렴한 6개 객실 조회
     @GetMapping("/cheapest-top6")
     public List<CheapestRoomWithImagesDTO> getTop6CheapestRooms() {
         return roomService.getTop6CheapestRooms();
     }
-
 }
